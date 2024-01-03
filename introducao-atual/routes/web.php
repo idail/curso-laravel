06954c3cb90243d985b056b41086b467eda814a5
idail <neto_br_8@hotmail.com>
2024-01-02 20:18:39 -0400

    Primeiro curso pacote laravel

---
+++ introducao-atual/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutosController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', HomeController::class);
+
+Route::get('produtos/inserir', [ProdutosController::class,"create"])->name("produtos.inserir");
+
+Route::get('produtos/{id}', [ProdutosController::class,"show"])->name("produtos.descricao");
+
+//Route::get('produtos/{nome}/{valor?}', [ProdutosController::class,"show"])->name("produtos.descricao");
+
+Route::get('produtos', [ProdutosController::class,"index"])->name("produtos");
+
+Route::post('produtos', [ProdutosController::class,"insert"])->name("produtos.insert");
+
+Route::get('users', function () {
+    return "usuário";
+});
